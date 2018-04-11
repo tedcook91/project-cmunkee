7250db70ee8e3322b5bfc1204aedbb14649e80fb
captainangus <edward.w.cook@gmail.com>
2018-04-11 13:05:49 -0400

    db fix

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,15 +1,7 @@
 require 'pry'
 
 class StocksController < ApplicationController
-    skip_before_action :verify_authenticity_token
-    # before_action :set_stock, only: [:show]
-    # def index
-    #     symbol = 'AT'
-    #     key = ENV['ALPHA_KEY']
-    #     response = HTTParty.get(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=#{symbol}&outputsize=full&apikey=#{key}`)
-    #     @stock = JSON.parse(response.body)
-    #     render json: @stock
-    # end
+
 
     def index
         @stocks = Stock.all
@@ -21,15 +13,23 @@ class StocksController < ApplicationController
 
     def create
         @stock = Stock.new(stock_params)
-        @stock.save
-        render :new
+        @stock.save!
+        render :show
     end
 
     def show
       key = ENV['ALPHA_KEY']
-      date = "2017-04-03"
-      @stock = 'MSFT'
-      stock_data = HTTParty.get("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=#{@stock}&outputsize=full&apikey=#{key}")      
+      symbol = @stock.symbol
+      date = @stock.date
+      stock_data = HTTParty.get("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=#{symbol}&outputsize=full&apikey=#{key}")      
         @close = stock_data["Time Series (Daily)"]["#{date}"]["4. close"]    
     end
+
+    
+    private
+    
+    def stock_params
+    params.require(:stock).permit(:symbol, :date)
+    end
+
 end
