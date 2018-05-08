d3e407cda8e59ccfa641dca1019d2cefa56eb0f0
captainangus <27704548+CaptainAngus@users.noreply.github.com>
2018-05-08 11:10:52 -0400

    allow line breaks

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -8,8 +8,8 @@ require 'open-uri'
 
 class StocksController < ApplicationController
 
-    
-    
+
+
     def index
         @stocks = Stock.all
     end
@@ -23,44 +23,49 @@ class StocksController < ApplicationController
         @stock = Stock.new(stock_params)
         key = ENV['ALPHA_KEY']
         symbol = @stock.symbol
-        symbols = symbol.split(',').collect(&:strip)
 
-        
-        
-        
+
+
+        if symbol.include?(",")
+          symbols = symbol.split(',').collect(&:strip)
+        else
+          symbols = symbol.split(/[\r\n]+/)
+        end
+
+
         date = @stock.date
 
         @symbol_array = []
-        
+
         symbols.each do |s|
-            
+
             puts "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=#{s}&outputsize=full&apikey=#{key}"
 
-            stock_data = HTTParty.get("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=#{s}&outputsize=full&apikey=#{key}")  
-            
-            
-            
-            
-            @close = stock_data["Time Series (Daily)"]["#{date}"]["4. close"]   
-             
-            
-            
+            stock_data = HTTParty.get("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=#{s}&outputsize=full&apikey=#{key}")
+
+
+
+
+            @close = stock_data["Time Series (Daily)"]["#{date}"]["4. close"]
+
+
+
 
             page_source = open("https://finance.yahoo.com/quote/#{s}/profile").read
-            
+
             parse_page = Nokogiri::HTML(page_source)
-            
+
             @name = parse_page.xpath('//section/div[1]/div/h3').text
             @sector = parse_page.xpath('//section/div[1]/div/div/p[2]/strong[1]').text
             @industry = parse_page.xpath('//section/div[1]/div/div/p[2]/strong[2]').text
-            
-            
+
+
            @symbol_array << {'symbol': s, 'name': @name, 'date': date, 'close': @close, 'sector': @sector, 'industry': @industry}
-            
+
         end
-            
+
             @symbol_array.flatten!
-        
+
 
         render :show
     end
@@ -68,9 +73,9 @@ class StocksController < ApplicationController
     def show
     end
 
-    
+
     private
-    
+
     def stock_params
     params.require(:stock).permit(:symbol, :date)
     end
